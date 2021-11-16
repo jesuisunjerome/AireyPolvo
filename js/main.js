3e4806b6bb4bf24f85ee2384cd17f0f94456f2c1
jesuisunjerome <38988287+jesuisunjerome@users.noreply.github.com>
2021-11-16 00:45:20 -0600

    AOS

--- js/main.js
+++ js/main.js
@@ -1,52 +1,57 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict'
+  AOS.init({
+      duration: 1000, // values from 0 to 3000, with step 50ms
+      offset: 0, // offset (in px) from the original trigger point
+  });
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+  (function () {
+      'use strict'
 
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.querySelectorAll('.needs-validation')
 
-                form.classList.add('was-validated')
-            }, false)
-        })
-})()
+      // Loop over them and prevent submission
+      Array.prototype.slice.call(forms)
+          .forEach(function (form) {
+              form.addEventListener('submit', function (event) {
+                  if (!form.checkValidity()) {
+                      event.preventDefault()
+                      event.stopPropagation()
+                  }
 
-
-function mostrarTab(el) {
-    var cardTab = $("#myTab .card-tab");
-    cardTab.removeClass("active");
-    $(el).addClass("active");
-
-    var tabPane = $("#experiencia .tab-pane");
-    tabPane.removeClass("active show");
-    var pane = "#" + el.id + "-tab";
-    $(pane).addClass("active show")
-}
+                  form.classList.add('was-validated')
+              }, false)
+          })
+  })()
 
 
-var tl = gsap.timeline();
+  function mostrarTab(el) {
+      var cardTab = $("#myTab .card-tab");
+      cardTab.removeClass("active");
+      $(el).addClass("active");
 
-tl.from(".intro-desc", .5, {
-        opacity: 0,
-        y: -50
-    })
-    .from(".intro-desc .titulo-1", .5, {
-        y: -20,
-        opacity: 0
-    })
-    .from(".intro-desc p", 1, {
-        y: 20,
-        opacity: 0
-    })
-    .from(".intro-img", 1, {
-        x: -200,
-        opacity: 0
-    }, "-=1");
+      var tabPane = $("#experiencia .tab-pane");
+      tabPane.removeClass("active show");
+      var pane = "#" + el.id + "-tab";
+      $(pane).addClass("active show")
+  }
+
+
+  var tl = gsap.timeline();
+
+  tl.from(".intro-desc", .5, {
+          opacity: 0,
+          y: -50
+      })
+      .from(".intro-desc .titulo-1", .5, {
+          y: -20,
+          opacity: 0
+      })
+      .from(".intro-desc p", 1, {
+          y: 20,
+          opacity: 0
+      })
+      .from(".intro-img", 1, {
+          x: -200,
+          opacity: 0
+      }, "-=1");
